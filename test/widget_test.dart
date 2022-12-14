29187b3bc42d0834d128726e3f7b873d66828b5c
mtvk100968 <tirupathim@gmail.com>
2022-12-14 22:30:20 +0530

    user current location not happening

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:landandplot/main.dart';
+import 'package:landandplot/homepage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
